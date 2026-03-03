5eee68bb4f78d53557e8ed2ccbbac88c353ec37d
Mengdi Wang <92598321+Edify0991@users.noreply.github.com>
2026-03-04 01:40:44 +0800

    Add zqsa01 residual PPO config and task registration

--- legged_gym/envs/__init__.py
+++ legged_gym/envs/__init__.py
@@ -32,6 +32,7 @@ from legged_gym import LEGGED_GYM_ROOT_DIR, LEGGED_GYM_ENVS_DIR
 from .base.legged_robot import LeggedRobot
 from .zqsa01.zqsa01 import ZqSA01
 from .zqsa01.zqsa01_config import ZqSA01Cfg, ZqSA01CfgPPO
+from .zqsa01.zqsa01_residual_config import ZqSA01ResidualCfgPPO
 
 import os
 
@@ -39,3 +40,4 @@ from legged_gym.utils.task_registry import task_registry
 
 task_registry.register("zqsa01", ZqSA01, ZqSA01Cfg(), ZqSA01CfgPPO())
 
+task_registry.register("zqsa01_residual", ZqSA01, ZqSA01Cfg(), ZqSA01ResidualCfgPPO())
